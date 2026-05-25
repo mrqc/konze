a43fb2814f979e998d69465122738bd61ccbecad
Erhard Dinhobl <er@master-studios.net>
2026-05-25 14:24:52 +0200

    spring boot impl showcasing + testing

--- example-spring-boot-app/src/main/kotlin/net/masterstudios/konze/examplespringboot/TestService.kt
+++ example-spring-boot-app/src/main/kotlin/net/masterstudios/konze/examplespringboot/TestService.kt
@@ -3,31 +3,41 @@ package net.masterstudios.konze.examplespringboot
 import net.masterstudios.konze.spring.DataSourceContextHolder.clearDataSourceType
 import net.masterstudios.konze.spring.DataSourceContextHolder.setDataSourceType
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 @Service
 class TestService(private val testRepository: TestRepository) {
-
-    fun process() {
-        val a = TestEntity(id = UUID.randomUUID(), name = "Some Name")
-        setDataSourceType("read-only-profile")
-        try {
-            testRepository.save(a)
-        } catch (e: Exception) {
-            println("Exception while saving: ${e.message}")
-        }
+    
+    @Transactional
+    fun fullAccessProfile() {
+        // Do a read
         testRepository.findAll()
-        setDataSourceType("write-only-profile")
-        testRepository.save(a)
-        try {
-            testRepository.findAll()
-        } catch (e: Exception) {
-            println("Exception while reading: ${e.message}")
-        }
-        
-        setDataSourceType("full-access-profile")
-        testRepository.findAll()
+        // Do a delete
         testRepository.deleteAll()
-        clearDataSourceType()
+    }
+
+    @Transactional
+    fun doASave() {
+        // Generate a test entity instance
+        val a = TestEntity(id = UUID.randomUUID(), name = "Some Name")
+        // Store the test entity instance, which works now
+        testRepository.save(a)
+    }
+
+    @Transactional
+    fun doARead() {
+        // Test the read only profile to access the database
+        testRepository.findAll()
+    }
+
+    @Transactional
+    fun doReadAndWrite() {
+        // Test the read only profile to access the database
+        testRepository.findAll()
+        // Generate a test entity instance
+        val a = TestEntity(id = UUID.randomUUID(), name = "Some Name")
+        // Store the test entity instance, which works now
+        testRepository.save(a)
     }
 }
--- example-spring-boot-app/src/test/kotlin/net/masterstudios/konze/examplespringboot/TestServiceIntegrationTest.kt
+++ example-spring-boot-app/src/test/kotlin/net/masterstudios/konze/examplespringboot/TestServiceIntegrationTest.kt
@@ -1,21 +1,67 @@
 package net.masterstudios.konze.examplespringboot
 
+import net.masterstudios.konze.spring.DataSourceContextHolder.clearDataSourceType
+import net.masterstudios.konze.spring.DataSourceContextHolder.setDataSourceType
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertFalse
 
 @SpringBootTest
 class TestServiceIntegrationTest {
 
     @Autowired
     lateinit var testService: TestService
-
+    
     @Test
-    fun `test process method handles exception in read-only profile`() {
+    fun `test process method handles exception in profile`() {
+        // Set the database access to read only profile
+        setDataSourceType("read-only-profile")
+        testService.doARead()
         try {
-            testService.process()
+            testService.doASave()
+            assertFalse(true, "doASave should throw an exception when using a read-only profile")
+        } catch (e: Exception) {
+            println("Caught expected exception when trying to save with read-only profile: ${e.message}")
+        }
+        try {
+            testService.doReadAndWrite()
+            assertFalse(true, "doReadAndWrite should throw an exception when using a read-only profile")
+        } catch (e: Exception) {
+            println("Caught expected exception when trying to read and write with read-only profile: ${e.message}")
+        }
+
+        // Switch the profile to write only
+        setDataSourceType("write-only-profile")
+        testService.doASave()
+        try {
+            testService.doARead()
+            assertFalse(true, "doARead should throw an exception when using a write only-profile")
+        } catch (e: Exception) {
+            println("Caught expected exception when trying to save with write only-profile")
+        }
+        try {
+            testService.doReadAndWrite()
+            assertFalse(true, "doReadAndWrite should throw an exception when using a read-only profile")
+        } catch (e: Exception) {
+            println("Caught expected exception when trying to read and write with read-only profile: ${e.message}")
+        }
+
+        // Switch to a full access profile
+        setDataSourceType("full-access-profile")
+        testService.fullAccessProfile()
+
+        clearDataSourceType()
+    }
+    
+    @Test
+    fun test() {
+        try {
+            this.`test process method handles exception in profile`()
         } catch (e: Exception) {
             println("Test caught unexpected exception: ${e.message}")
+            assertFalse(true, "process method should handle exceptions internally and not throw them")
         }
     }
 }
